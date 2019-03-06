258a5693d43f944872324dc30307023f9ea340e0
Prahas Vichakshana <pvfrozty.wolf@gmail.com>
2019-03-06 22:22:22 +0530

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       asd
+      </div>
+    );
+  }
+}
+
+export default App;
